dfb13467bb8ead0070bc1935b0aa0b699fe320be
Indorkar-29 <119936657+Indorkar-29@users.noreply.github.com>
2023-02-01 00:47:50 +0530

    Create Inventory Updated

--- routes/inventory.js
+++ routes/inventory.js
@@ -2,32 +2,28 @@ const express=require('express');
 const inventoryModel=require('../models/inventoryModel');
 const router=express.Router();
 
-router.post('/createInventory',(req,res)=>{
-    // const data=inventoryModel.find({item_name:req.body.item_name});
-    // console.log(data);
-    //     if(data){
-    //         const newQuantity=(data[0].available_quantity)+(req.body.available_quantity);
-    //         inventoryModel.updateOne({inventory_id:req.body.inventory_id},{$set:{available_quantity:newQuantity}}).then(()=>{
-    //             res.status(200).send("Inventory Updated Successfully");
-    //         }).catch((err)=>{
-    //             res.status(400).send(err.message);
-    //         });
-    //     }else{
+router.post('/createInventory',async (req,res)=>{
+    try{
+        const item=await inventoryModel.find({item_name:req.body.item_name})
+        if(item.length){
+            const newQuantity=item[0].available_quantity + req.body.available_quantity;
+            await inventoryModel.updateOne({item_name:req.body.item_name},{$set:{available_quantity:newQuantity}});
+            res.status(200).send("Inventory Updated Successfully");
+        }else{
             inventoryModel.create({
-                inventory_type:req.body.inventory_type,
-                item_name:req.body.item_name,
-                available_quantity:req.body.available_quantity
-            }).then(()=>{
-                res.status(200).send("New Inventory Created Successfully");
-            }).catch((err)=>{
-                res.status(400).send(err.message);
-            });
+                        inventory_type:req.body.inventory_type,
+                        item_name:req.body.item_name,
+                        available_quantity:req.body.available_quantity
+                    }).then(()=>{
+                        res.status(200).send("New Inventory Created Successfully");
+                    }).catch((err)=>{
+                        res.status(400).send(err.message);
+                    });
         }
-    // }).catch((err)=>{
-    //     res.status(400).send(err.message);
-    // });
-// }
-);
+    }catch(e){
+        res.status(400).send(e.message);
+    }
+});
 
 router.get("/inventory",(req,res)=>{
     inventoryModel.find().then((data)=>{
